439b0d4e85a33f151f44ed7d06cada1f0aa49c1b
antonyourkevich <antonyourkevich@tut.by>
2019-04-25 21:27:10 +0300

    vol.2 i.khlebovets

--- src/IG/les1/Task1.java
+++ src/IG/les1/Task1.java
@@ -5,8 +5,8 @@ public class Task1
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!1");
-        System.out.println("Hello World!2");
+        System.out.println("Hello World!11");
+        System.out.println("Hello World!22");
         System.out.println("Hello World!3");
         System.out.println("Hello World!4");
         System.out.println("Hello World!j555g");
