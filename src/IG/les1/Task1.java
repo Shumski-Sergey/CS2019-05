65ea621a84dd97c96422e3a18d9e508a7ab474a2
antonyourkevich <antonyourkevich@tut.by>
2019-04-25 21:18:33 +0300

    выполнил таск1 и таск2

---
+++ src/IG/les1/Task1.java
@@ -0,0 +1,23 @@
+package IG.les1;
+
+public class Task1
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!1");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!3");
+        System.out.println("Hello World!4");
+        System.out.println("Hello World!j555g");
+
+
+
+
+
+        int i= 4;
+        int j=3;
+        int k=j*j+i*i;
+        System.out.println(j+"*"+j+"+"+i+"*"+i+"="+k);
+    }
+}
