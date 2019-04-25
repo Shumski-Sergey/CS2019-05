e76e21cf67224f43a6042b6e1ab7270d97d7e323
lazytazly <amafairmylazar@gmail.com>
2019-04-25 21:34:02 +0300

    ovchinko 3 tacks change

--- src/p/ovchinko/Task3.java
+++ src/p/ovchinko/Task3.java
@@ -4,8 +4,8 @@ public class Task3 {
     public static void main(String[] args) {
         int i=2;
         int j=3;
-        int k=i+j;
-        System.out.println(i+"+"+j+"="+k);
+        int k=i+j+j;
+        System.out.println(i+"+"+j*2+"="+k);
 
     }
 }
