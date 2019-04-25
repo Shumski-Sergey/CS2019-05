308eb17e572b43764a01eef18956add324249444
sshumsky <shumski.s.a@gmail.com>
2019-04-25 21:28:09 +0300

    Change task1

--- src/sshumsky/lesson1/Task1.java
+++ src/sshumsky/lesson1/Task1.java
@@ -3,6 +3,6 @@ package sshumsky.lesson1;
 public class Task1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!!!!!!!!");
+        System.out.println("Hello World!!!!!!!!!!!!!");
     }
 }
