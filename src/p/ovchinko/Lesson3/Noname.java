9c7a94d1c399b1ab9445f87ae57db1c1eacdaad3
lazytazly <amafairmylazar@gmail.com>
2019-04-30 14:59:05 +0300

    3 lesson hometask

---
+++ src/p/ovchinko/Lesson3/Noname.java
@@ -0,0 +1,27 @@
+package p.ovchinko.Lesson3;
+
+public class Noname {
+    public static void main(String[] args) {
+        if (12<11)        System.out.println("lol");
+
+        int a,j;
+        a=10;
+        j=11;
+
+        if (!(a==j)) System.out.println(123);
+        if (a!=j) System.out.println(987);
+
+
+        System.out.println("10>9 это " + (10>9));
+
+        char z;
+        z='X';
+        z++; z++;
+        System.out.println(z);
+
+        int u,i,o;
+        u=i=o=5;
+        u+=10;
+        System.out.println(u+i+o);
+}
+}
