8290217208a5b663f90964869195ca59ea95846d
yuraej <yuraej@gmail.com>
2019-04-30 12:43:18 +0300

    home work of lessons 1-3 is done

--- src/yuraMalish/Les2TaskB1.java
+++ src/yuraMalish/Les2TaskB1.java
@@ -2,7 +2,7 @@ package yuraMalish;
 import java.util.Scanner;
 public class Les2TaskB1 {
     public static void main(String[] args){
-        System.out.println("Введите число целое число");
+        System.out.println("Введите целое число");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println("Квадрат вашего числа равен" + " " +(n*n) );
