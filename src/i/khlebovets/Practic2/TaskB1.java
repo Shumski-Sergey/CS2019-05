240b2b02780bf12cb68996cf423d831e25597037
sshumsky <shumski.s.a@gmail.com>
2019-04-30 18:15:02 +0300

    Fix merge commit

--- src/i/khlebovets/Practic2/TaskB1.java
+++ src/i/khlebovets/Practic2/TaskB1.java
@@ -1,44 +1,55 @@
 package i.khlebovets.Practic2;
 
-
-
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-
 import java.util.Scanner;
 
-public class TaskB1
-{
-    public class TaskA1
-    {
 
-        public static double dis (int a, int b, int c)
-        {
-            int k= Math.sqrt(a);
-            System.out.println("ВЫ ВВЕЛИ  "+a);
-            int l= Math.sqrt(b);
-            System.out.println("ВЫ ВВЕЛИ  "+b);
-            int m= Math.sqrt(c);
-            System.out.println("ВЫ ВВЕЛИ  "+c);
-            return (double)k, (double)l, (double)m;
+public class TaskB1 {
 
-        }
+    private static double dis(int a, int b, int c) {
+        double k = a;
+        System.out.println("ВЫ ВВЕЛИ  " + a);
+        double l = b;
+        System.out.println("ВЫ ВВЕЛИ  " + b);
+        double m = c;
+        System.out.println("ВЫ ВВЕЛИ  " + c);
 
+        double d;
+        d = l * l - 4 * k * m;
 
-        public static void main(String[] args)
-        {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Введите 1ое число!!!");
-            int a=sc.nextInt();
-            System.out.println("Введите 2ое число!!!");
-            int b=sc.nextInt();
-            System.out.println("Введите 3ое число!!!");
-            int c=sc.nextInt();
-
-            dis(a,b,c);
+        System.out.println("Дискриминант равен\t" + d);
 
 
-            System.out.println();
+        return d;
 
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 1ое число!!!");
+        int a = sc.nextInt();
+        System.out.println("Введите 2ое число!!!");
+        int b = sc.nextInt();
+        System.out.println("Введите 3ое число!!!");
+        int c = sc.nextInt();
+
+        double d = dis(a, b, c);
+
+        if (d > 0) {
+            double x1, x2;
+
+            x1 = (-b + Math.sqrt(d)) / (2 * a);
+            x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+            System.out.println("Первый корень: " + x1 + " Второй корень: " + x2);
+        } else if (d == 0) {
+            double x;
+            x = -b / (2 * a);
+
+            System.out.println("Дискриминант имеет один корень: " + x);
+        } else {
+            System.out.println("Отрицательный Дискриминант");
         }
     }
 }
+
