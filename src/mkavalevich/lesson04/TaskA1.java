8da7c26d1e7ecb636559fb8a9f53042788009224
mkavalevich <ritavalerievna@gmail.com>
2019-05-02 00:25:16 +0200

    Lesson04 (Task A1,A2,A3,B1) completed

---
+++ src/mkavalevich/lesson04/TaskA1.java
@@ -0,0 +1,14 @@
+package mkavalevich.lesson04;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+        int x;
+        for (x=1; x<=10; x++)
+        if ((x==3) || (x==9)) {
+            System.out.println("Меня интересует высокая зарплата!");
+        }
+        else {
+        System.out.println("Я изучаю программирование.");
+        }
+    }
+}
